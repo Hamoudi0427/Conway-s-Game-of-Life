43373394c13a450e4676a06ef5c27e2076c57ea6
Hamoudi0427 <68081210+Hamoudi0427@users.noreply.github.com>
2021-05-22 22:10:42 -0400

    Add files via upload

---
+++ Src/conway.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "helper.h"
+#include <SDL.h>
+
+
+int main(int argc, char* argv[])
+{
+    //Intro
+    printf("Welcome to Conway's Game of Life!\n");
+    printf("There are two rules:\n");
+    printf("1) A live cell remains alive if it has two or three neighbours. \n");
+    printf("2) A dead cell becomes alive if it has exactly three neighbours. \n");
+    printf("You can press the left arrow key to slow down the simulation or the right arrow key to speed up the simulation. \n");
+    SDL_Delay(7000);
+    printf("The simulation will now begin.\n");
+    SDL_Delay(2000);
+    
+    //simulation options, with random generation being the default
+    int start_pos = 2;
+    if (argv[1] == 'g')
+    {
+        //glider
+        start_pos = 1;
+    }
+    else if (argv[1] == 'gg')
+    {
+        //glider gun
+        start_pos = 2;
+    }
+    else if (argv[2] == 'ss')
+    {
+        //space ship
+        start_pos = 3;
+    }
+    
+    //SDL initialization
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        fprintf(stderr, "Could not initialize SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+    SDL_Window* screen = SDL_CreateWindow("Conway's Game of Life", 
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        500, 500,
+        0);
+    if (!screen) {
+        fprintf(stderr, "Could not create window\n"); 
+        return 1;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_SOFTWARE); //creates renderer (part that draws)
+    if (!renderer) {
+        fprintf(stderr, "Could not create renderer\n");
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255); //sets renderer colour
+    SDL_RenderClear(renderer); //clears window
+    SDL_RenderPresent(renderer); //renders image on screen (draws)
+
+    //simulation initialization
+    int map_array[100][100] = { 0 };
+    int generation = 0;
+    int* generation_p = &generation;
+    int population = 0;
+    int* population_p = &population;
+    int run_sim = 0;
+    int sim_speed = 300;
+
+    //initializes map and population
+    init_map(map_array, start_pos);
+    count_pop(map_array, population_p);
+
+    //running simulation
+    while (run_sim == 0)
+    {
+        for (int y = 0; y < MAP_SIZE; y++)
+        {
+            for (int x = 0; x < MAP_SIZE; x++)
+            {
+                //create a rectangle 
+                SDL_Rect cell;
+                cell.x = (x * 5);
+                cell.y = (5 * y);
+                cell.w = 5;
+                cell.h = 5;
+
+                //check if cell is alive or dead
+                if (map_array[y][x] == 0)
+                {
+                    SDL_SetRenderDrawColor(renderer, 242, 242, 242, 255);
+                }
+                else
+                {
+                    SDL_SetRenderDrawColor(renderer, 211, 146, 175, 255);
+                }
+
+                //fill rectangle
+                SDL_RenderFillRect(renderer, &cell);
+            }
+        }
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(sim_speed);
+        SDL_RenderClear(renderer);
+        update_map(map_array, generation_p, population_p);
+
+        //processes quitting and simulation speed 
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                run_sim = 1;
+                printf("Amount of Simulated Generations: %d\n", generation);
+                printf("Final population: %d\n", population);
+                printf("Thank you for playing Conway's Game of Life! Perhaps try another simulation.\n");
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_LEFT:
+                    if (sim_speed < 500)
+                    {
+                        sim_speed += 100;
+                    }
+                    break;
+                case SDLK_RIGHT: 
+                    if (sim_speed > 100)
+                    {
+                        sim_speed -= 100;
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+            
+        }
+        
+    }
+    
+    //closes SDL window and quits
+    SDL_Delay(1000);
+    SDL_DestroyWindow(screen);
+    SDL_Quit();
+    return 0;
+}
+
---
+++ Src/conway_helper.c
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "helper.h"
+#include <SDL.h>
+
+
+//intializes map based on start position input
+void init_map(int map_array[100][100], int start_position)
+{
+    if (start_position == 0)
+    {
+        //randomize starting population
+        srand(time(NULL));
+        int pop = rand() % (MAP_SIZE * MAP_SIZE);
+
+        //randomize starting positions
+        for (int i = 0; i < pop; i++)
+        {
+            int x = rand() % (MAP_SIZE - 1);
+            int y = rand() % (MAP_SIZE - 1);
+
+            map_array[x][y] = 1;
+        }
+    }
+    else if (start_position == 1)
+    {
+        //glider 
+        map_array[6][15] = 1;
+        map_array[6][16] = 1;
+        map_array[6][17] = 1;
+        map_array[5][17] = 1;
+        map_array[4][16] = 1;
+
+    }
+    else if (start_position == 2)
+    {
+        //glider gun
+        map_array[18][1] = 1;
+        map_array[18][2] = 1;
+        map_array[19][1] = 1;
+        map_array[19][2] = 1;
+        map_array[18][11] = 1;
+        map_array[19][11] = 1;
+        map_array[20][11] = 1;
+        map_array[17][12] = 1;
+        map_array[21][12] = 1;
+        map_array[16][13] = 1;
+        map_array[16][14] = 1;
+        map_array[22][13] = 1;
+        map_array[22][14] = 1;
+        map_array[19][15] = 1;
+        map_array[17][16] = 1;
+        map_array[21][16] = 1;
+        map_array[18][17] = 1;
+        map_array[19][17] = 1;
+        map_array[20][17] = 1;
+        map_array[19][18] = 1;
+        map_array[16][21] = 1;
+        map_array[17][21] = 1;
+        map_array[18][21] = 1;
+        map_array[16][22] = 1;
+        map_array[17][22] = 1;
+        map_array[18][22] = 1;
+        map_array[15][23] = 1;
+        map_array[19][23] = 1;
+        map_array[14][25] = 1;
+        map_array[15][25] = 1;
+        map_array[19][25] = 1;
+        map_array[20][25] = 1;
+        map_array[16][35] = 1;
+        map_array[17][35] = 1;
+        map_array[16][36] = 1;
+        map_array[17][36] = 1;
+    }
+    else if (start_position == 3)
+    {
+        //space ship
+        map_array[62][33] = 1;
+        map_array[62][34] = 1;
+        map_array[62][35] = 1;
+        map_array[62][37] = 1;
+        map_array[63][37] = 1;
+        map_array[64][37] = 1;
+        map_array[61][38] = 1;
+        map_array[61][39] = 1;
+        map_array[61][40] = 1;
+        map_array[63][41] = 1;
+        map_array[66][40] = 1;
+        map_array[67][40] = 1;
+        map_array[71][40] = 1;
+        map_array[68][41] = 1;
+        map_array[70][41] = 1;
+        map_array[72][41] = 1;
+        map_array[67][42] = 1;
+        map_array[68][42] = 1;
+        map_array[64][43] = 1;
+        map_array[65][43] = 1;
+        map_array[68][43] = 1;
+        map_array[69][43] = 1;
+        map_array[63][44] = 1;
+        map_array[65][44] = 1;
+        map_array[66][44] = 1;
+        map_array[68][44] = 1;
+        map_array[62][45] = 1;
+        map_array[68][45] = 1;
+        map_array[62][46] = 1;
+        map_array[68][46] = 1;
+        map_array[63][47] = 1;
+        map_array[65][47] = 1;
+        map_array[66][47] = 1;
+        map_array[68][47] = 1;
+        map_array[64][48] = 1;
+        map_array[65][48] = 1;
+        map_array[68][48] = 1;
+        map_array[69][48] = 1;
+        map_array[67][49] = 1;
+        map_array[68][49] = 1;
+        map_array[63][50] = 1;
+        map_array[68][50] = 1;
+        map_array[70][50] = 1;
+        map_array[72][50] = 1;
+        map_array[61][51] = 1;
+        map_array[66][51] = 1;
+        map_array[67][51] = 1;
+        map_array[71][51] = 1;
+        map_array[61][52] = 1;
+        map_array[61][53] = 1;
+        map_array[62][54] = 1;
+        map_array[63][54] = 1;
+        map_array[64][54] = 1;
+        map_array[62][56] = 1;
+        map_array[62][57] = 1;
+        map_array[62][58] = 1;
+    }
+
+}
+
+
+//updates map to next generation 
+void update_map(int map_array[100][100], int* generation_p, int* pop)
+{
+    //creates new temp array
+    int tmp[100][100] = {0};
+
+    //updates map 
+    for (int i = 0; i < MAP_SIZE; i++)
+    {
+        for (int j = 0; j < MAP_SIZE; j++)
+        {
+            int count_neighbour = 0;
+
+            for (int a = 0; a < 3; a++)
+            {
+                for (int b = 0; b < 3; b++)
+                {
+                    //continues if it is out of array bounds
+                    if (i - 1 + a < 0 || i - 1 + a > MAP_SIZE - 1 || j - 1 + b < 0 || j - 1 + b > MAP_SIZE - 1)
+                    {
+                        continue;
+                    }
+
+                    //counts value of neighbour
+                    count_neighbour += map_array[i - 1 + a][j - 1 + b];
+                }
+            }
+
+            //update tmp based on if live or dead
+            if (map_array[i][j] == 0)
+            {
+                if (count_neighbour == 3)
+                {
+                    tmp[i][j] = 1;
+                }
+            }
+            else
+            {
+                if (count_neighbour == 3 || count_neighbour == 4)
+                {
+                    tmp[i][j] = 1;
+                }
+                else
+                {
+                    tmp[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    //copies tmp back to map
+    copy_map(map_array, tmp);
+
+    //updates generation
+    *generation_p += 1;
+
+    //updates population count
+    count_pop(map_array, pop);
+
+}
+
+//transfer tmp map back to original
+void copy_map(int map_array[100][100], int tmp_array[100][100])
+{
+    for (int i = 0; i < MAP_SIZE; i++)
+    {
+        for (int j = 0; j < MAP_SIZE; j++)
+        {
+            if (tmp_array[i][j] == 1)
+            {
+                map_array[i][j] = 1;
+            }
+            else
+            {
+                map_array[i][j] = 0;
+            }
+        }
+    }
+}
+
+//counts population
+void count_pop(int map_array[100][100], int* pop)
+{
+    int current_pop = 0;
+
+    for (int i = 0; i < MAP_SIZE; i++)
+    {
+        for (int j = 0; j < MAP_SIZE; j++)
+        {
+            current_pop += map_array[i][j];
+        }
+    }
+
+    *pop = current_pop;
+}
+
