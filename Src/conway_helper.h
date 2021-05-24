70759215efdcad1492e709115010e8029027d0a5
Hamoudi0427 <68081210+Hamoudi0427@users.noreply.github.com>
2021-05-23 23:39:32 -0400

    Add files via upload
    Updated header file.

---
+++ Src/conway_helper.h
@@ -0,0 +1,12 @@
+//function prototypes and constants
+#ifndef CONWAY_HELPER_H
+#define CONWAY_HELPER_H
+
+#define MAP_SIZE 100
+typedef int Map[MAP_SIZE][MAP_SIZE];
+void init_map(Map map_array, int start_position);
+void update_map(Map map_array, int* generation_p, int* pop);
+void copy_map(Map map_array, Map tmp_array);
+void count_pop(Map map_array, int* pop);
+
+#endif
