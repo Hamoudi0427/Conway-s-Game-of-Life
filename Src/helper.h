d77c84303b1bcc98d44501255fec1a8b3a1b6d54
Hamoudi0427 <68081210+Hamoudi0427@users.noreply.github.com>
2021-05-23 23:40:21 -0400

    Delete helper.h
    Deleted old header file.

--- Src/helper.h
+++
@@ -1,7 +0,0 @@
-//function prototypes and constants
-#define MAP_SIZE 100
-typedef int Map[MAP_SIZE][MAP_SIZE];
-void init_map(Map map_array, int start_position);
-void update_map(Map map_array, int* generation_p, int* pop);
-void copy_map(Map map_array, Map tmp_array);
-void count_pop(Map map_array, int* pop);
