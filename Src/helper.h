ec123301fb81ca65db5f0a18d0873d853f29d332
Hamoudi_0427 <hamoudi0427@gmail.com>
2021-05-23 22:47:59 -0400

    Changed array parameters in Map data type.

--- Src/helper.h
+++ Src/helper.h
@@ -1,6 +1,6 @@
 //function prototypes and constants
 #define MAP_SIZE 100
-typedef int Map[100][100];
+typedef int Map[MAP_SIZE][MAP_SIZE];
 void init_map(Map map_array, int start_position);
 void update_map(Map map_array, int* generation_p, int* pop);
 void copy_map(Map map_array, Map tmp_array);
