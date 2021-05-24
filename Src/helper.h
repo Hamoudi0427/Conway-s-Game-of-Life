f44839f4273a5300a9dea7c723860e60989d6922
Hamoudi_0427 <hamoudi0427@gmail.com>
2021-05-23 22:09:16 -0400

    Implemented Map data type.

--- Src/helper.h
+++ Src/helper.h
@@ -1,6 +1,7 @@
 //function prototypes and constants
 #define MAP_SIZE 100
-void init_map(int map_array[100][100], int start_position);
-void update_map(int map_array[100][100], int* generation_p, int* pop);
-void copy_map(int map_array[100][100], int tmp_array[100][100]);
-void count_pop(int map_array[100][100], int* pop);
+typedef int Map[100][100];
+void init_map(Map map_array, int start_position);
+void update_map(Map map_array, int* generation_p, int* pop);
+void copy_map(Map map_array, Map tmp_array);
+void count_pop(Map map_array, int* pop);
